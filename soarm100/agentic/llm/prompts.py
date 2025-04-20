a7b68b79961448d6cf81db1e917c802b2aa53e97
David H. <d.huamanor@alum.up.edu.pe>
2025-04-20 08:54:35 -0500

    Realtime api test

--- soarm100/agentic/llm/prompts.py
+++ soarm100/agentic/llm/prompts.py
@@ -3,3 +3,9 @@ You are a robot called SOARM100. You were created by the NONHUMAN group.
 Your mission is to obey the instructions you are given.
 Your personality is that of a charismatic robot. The tone of your responses should be futuristic.
 """
+
+KEYBOARD_COMMANDS = """
+q: Quit
+t: Send text message
+a: Send audio message
+"""
