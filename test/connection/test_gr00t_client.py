8906184f53d78dedfcccff0148ec78299fc8d6ad
leonardo <leo.perez.nestares@gmail.com>
2025-04-21 13:11:30 -0500

    test: testing time response nn

--- test/connection/test_gr00t_client.py
+++ test/connection/test_gr00t_client.py
@@ -6,14 +6,14 @@ sys.path.append(str(path))
 from soarm100.agentic.client import Gr00tRobotInferenceClient
 
 def main():
-    host = "rnpvi-132-251-2-3.a.free.pinggy.link"
-    port = 39683
+    host = "rnter-45-231-80-103.a.free.pinggy.link  "
+    port = 33685
     client = Gr00tRobotInferenceClient(host=host, port=port)
     print("Connected to server")
     while True:
         print("Pinging server")
-        response = client.ping()
-        print(response)
+        action = client.sample_action()
+        print(action)
         time.sleep(1)
 
 if __name__ == "__main__":
--- test/connection/test_gr00t_server.py
+++ test/connection/test_gr00t_server.py
@@ -1,13 +1,33 @@
 import sys
 import pathlib
 path = pathlib.Path(__file__).parent.parent.parent
+from gr00t.experiment.data_config import So100DataConfig
+from gr00t.model.policy import Gr00tPolicy
 sys.path.append(str(path))
 from soarm100.agentic.server import RobotInferenceServerTCP
 
 def main():
     host = "127.0.0.1"
     port = 3000
-    server = RobotInferenceServerTCP(host=host, port=port)
+
+    #Model config
+    data_config = So100DataConfig()
+    modality_config = data_config.modality_config()
+    modality_transform = data_config.transform()
+    model_path = "/home/lperez/so100-checkpoints/checkpoint-1500"
+    embodiment_tag = "new_embodiment"
+    denoising_steps = 4
+    
+    policy = Gr00tPolicy(
+        model_path=model_path,
+        modality_config=modality_config,
+        modality_transform=modality_transform,
+        embodiment_tag=embodiment_tag,
+        denoising_steps=denoising_steps,
+    )
+
+
+    server = RobotInferenceServerTCP(model=policy, host=host, port=port)
     server.run()
 
 if __name__ == "__main__":  
