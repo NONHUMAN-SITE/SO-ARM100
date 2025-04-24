95c681c1196499a45dbb6d0edc4f9f8e2061002b
leonardo <leo.perez.nestares@gmail.com>
2025-04-24 10:56:42 -0500

    clearning repo

--- soarm100/agentic/stt/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class STTBase(ABC):
-    def __init__(self):
-        pass
-
-    @abstractmethod
-    def transcribe(self, audio_data: bytes) -> str:
-        pass
